0a538f42c70704d3cac46917874e6a4bc9544439
alif riduwan <alifriduwan.a@gmail.com>
2023-04-06 01:06:38 +0700

    Method Object.create()

--- main.js
+++ main.js
@@ -1,11 +1,21 @@
-// Constructor
+// Method Object.create()
 
-function Car(color) {
-  this.color = color;
-  return true;
-}
+// structure Object.create(prototype, [propertiesObject])
+var car = {
+  // => prototype
+  drive: function () {
+    console.log("driving a car"); //ลูกๆของมันสามารถเรียกใช้ฟังก์ชันนี้ได้
+  },
+};
 
-var redCar = new Car("Red");
-var blueCar = new Car("Blue");
-console.log(redCar.color); //Red
-console.log(blueCar.color); //Blue
+var redCar = Object.create(car);
+var blackCar = Object.create(car, {
+  // add properties
+  foo: { writable: true, configurable: true, value: "fooValue" }, //descriptor
+  bar: { writable: true, configurable: true, value: "barValue" },
+});
+
+redCar.drive(); //driving a car
+blackCar.drive(); //driving a car
+console.log(blackCar.foo); //fooValue
+console.log(blackCar.bar); //barValue
