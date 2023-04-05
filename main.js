597774c2a4efefb72c6615e5ebbfafb295ad8ab0
alif riduwan <alifriduwan.a@gmail.com>
2023-04-05 13:47:00 +0700

    การประกาศ object

---
+++ main.js
@@ -0,0 +1,15 @@
+// Object
+// key : value
+
+// empty object
+
+var font = {};
+
+var fonts = {
+  color: "red", //key is color : value is red datatype string
+  myFunction: function (param) {}, // keyชื่อ myFunction : value is method
+  Option: {
+    //key is option , value is object ซ้อน object
+    value: 1,
+  },
+};
