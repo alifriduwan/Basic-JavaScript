fdc29d8b224c8b3e78e738c683461126885a27d4
alif riduwan <alifriduwan.a@gmail.com>
2023-04-12 21:51:12 +0700

    !------------------------------------------------!

--- Ch7_regexPart1.js
+++
@@ -1,5 +0,0 @@
-var re = /like/;
-console.log(re.exec("I like you")); //[ 'like', index: 2, input: 'I like you', groups: undefined ]
-
-var re2 = /html|div|br|button/;
-console.log(re2.exec("<div>")); //[ 'div', index: 1, input: '<div>', groups: undefined ]
--- ch8-Unicode.js
+++
@@ -1,9 +0,0 @@
-var a = "abc";
-console.log(a.length); //3
-console.log(a.codePointAt(0)); //97
-console.log(a.codePointAt(1)); //98
-console.log(a.codePointAt(2)); //99
-console.log(a.codePointAt(3)); //undefined
-
-// code point => string
-console.log(String.fromCodePoint(17)); //◄
